027d53985e157c0ae981a9211e7fc85935a51685
Himanshu Sharma <himanshu.sharma1103@gmail.com>
2020-07-11 22:33:06 +0530

    add coverage check and coverage threshold

--- src/containers/MockView/MockView.test.js
+++ src/containers/MockView/MockView.test.js
@@ -1,16 +1,17 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, cleanup } from "@testing-library/react";
 import MockView from "./MockView";
 import axios from "axios";
 
 jest.mock("axios");
+afterEach(cleanup);
 
 it("renders correctly", () => {
   const { baseElement } = render(<MockView />);
   expect(baseElement).toMatchSnapshot();
 });
 
-it("should dispatch request on click of fetch stubs button", () => {
+it("should fetch stubs from a given url on click fetch stubs button", () => {
   const testUrl = "http://test/5000";
   const response = {
     stubs: [
