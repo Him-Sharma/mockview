ad679b9420d69d2aebb6a01eed923b7cc7043558
Himanshu Sharma <himanshu.sharma1103@gmail.com>
2020-08-03 18:19:58 +0530

    Use functinal component for mockview instead of class based component

--- src/containers/MockView/MockView.js
+++ src/containers/MockView/MockView.js
@@ -1,64 +1,62 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./MockView.css";
 import StubItem from "../../components/StubListItem/StubListItem";
 import StubItemInfo from "../../components/StubItemInfo/StubItemInfo";
 
-class MockView extends Component {
-  state = {
-    stubItems: [],
-    selectedStubItem: null,
-  };
-  fetchStubHandler = () => {
+const MockView = () => {
+  const [stubItems, setStubItems] = useState([]);
+  const [selectedStubItem, setselectedStubItem] = useState(null);
+
+  const fetchStubHandler = () => {
     const url = document.getElementById("serverUrlInput").value;
     axios.get(url).then((response) => {
-      const stubItems = response.data.stubs;
-      const updateStubItems = stubItems.map((stubItem) => {
+      const rawStubItems = response.data.stubs;
+      const updateStubItems = rawStubItems.map((stubItem) => {
         return {
           ...stubItem,
           id: stubItem._links.self.href.split("/").pop(),
         };
       });
-      this.setState({ stubItems: updateStubItems });
+      setStubItems(updateStubItems);
     });
   };
-  stubItemSelectedHandler = (stubItem) => {
-    this.setState({ selectedStubItem: stubItem });
+  const stubItemSelectedHandler = (stubItem) => {
+    setselectedStubItem(stubItem);
   };
-  render() {
-    let stubItems = <h1>No stubitems to display!</h1>;
-    if (this.state.stubItems.length > 0) {
-      stubItems = this.state.stubItems.map((stubItem) => {
-        return (
-          <StubItem
-            endpointName={stubItem.predicates[0].equals.path}
-            method={stubItem.predicates[0].equals.method}
-            key={stubItem.id}
-            clicked={() => this.stubItemSelectedHandler(stubItem)}
-          />
-        );
-      });
-    }
 
-    return (
-      <div className="MockView">
-        <h1>Mockview</h1>
-        <section className="Control">
-          <input
-            type="text"
-            id="serverUrlInput"
-            data-testid="serverUrlInput"
-          ></input>
-          <button onClick={this.fetchStubHandler} data-testid="fetchStubButton">
-            Fetch Stubs
-          </button>
-        </section>
-        <section className="StubContainer">
-          <div className="StubItemList">{stubItems}</div>
-          <StubItemInfo selectedStubItem={this.state.selectedStubItem} />
-        </section>
-      </div>
-    );
+  let stubItemsElement = <h1>No stubitems to display!</h1>;
+  if (stubItems.length > 0) {
+    stubItemsElement = stubItems.map((stubItem) => {
+      return (
+        <StubItem
+          endpointName={stubItem.predicates[0].equals.path}
+          method={stubItem.predicates[0].equals.method}
+          key={stubItem.id}
+          clicked={() => stubItemSelectedHandler(stubItem)}
+        />
+      );
+    });
   }
-}
+
+  return (
+    <div className="MockView">
+      <h1>Mockview</h1>
+      <section className="Control">
+        <input
+          type="text"
+          id="serverUrlInput"
+          data-testid="serverUrlInput"
+        ></input>
+        <button onClick={fetchStubHandler} data-testid="fetchStubButton">
+          Fetch Stubs
+        </button>
+      </section>
+      <section className="StubContainer">
+        <div className="StubItemList">{stubItemsElement}</div>
+        <StubItemInfo selectedStubItem={selectedStubItem} />
+      </section>
+    </div>
+  );
+};
 export default MockView;
