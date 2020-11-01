e7e3a28dc2dc49da222940ef3737528111bf11c9
Himanshu Sharma <himanshu.sharma1103@gmail.com>
2020-11-01 11:32:04 +0530

    UseEffect to load stubs when mockserver url is changed

--- src/components/StubList/StubList.js
+++ src/components/StubList/StubList.js
@@ -6,9 +6,9 @@ const StubList = (props) => {
     <section className="stub-list">
       <ul>
         {props.stubs.map((stub) => (
-          <li key={stub.id} onClick={props.selected.bind(this, stub.id)}>
-            <span>{stub.method}</span>
-            <span>{stub.endpointName}x</span>
+          <li key={stub.id}>
+            <span>{stub.predicates[0].equals.method}</span>
+            <span>{stub.predicates[0].equals.path}</span>
           </li>
         ))}
       </ul>
--- src/containers/MockView/MockView.js
+++ src/containers/MockView/MockView.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Aux from "../../hoc/Aux";
 import Control from "../../components/Control/Control";
@@ -8,23 +8,26 @@ const MockView = () => {
   const [stubs, setStubs] = useState([]);
   const [mockServerURL, setMockServerURL] = useState("");
 
+  useEffect(() => {
+    if (mockServerURL) {
+      axios.get(mockServerURL).then((response) => {
+        console.log(response);
+        const rawStubItems = response.data.stubs;
+        const updatedStubs = rawStubItems.map((stubItem) => {
+          return {
+            ...stubItem,
+            id: stubItem._links.self.href.split("/").pop(),
+          };
+        });
+        console.log(updatedStubs);
+        setStubs(updatedStubs);
+      });
+    }
+  }, [mockServerURL]);
   const urlSubmitHandler = (mockServerURL) => {
     setMockServerURL(mockServerURL);
-    _fetchStubs();
   };
-  const _fetchStubs = () => {
-    axios.get(mockServerURL).then((response) => {
-      console.log(response);
-      const rawStubItems = response.data;
-      const updateStubs = rawStubItems.map((stubItem) => {
-        return {
-          ...stubItem,
-          id: stubItem._links.self.href.split("/").pop(),
-        };
-      });
-      setStubs(updateStubs);
-    });
-  };
+
   return (
     <Aux>
       <Control onFormSubmit={urlSubmitHandler} />
