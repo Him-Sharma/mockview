1191e62cc75e1e442b2297c90d140e8965c74b6c
Himanshu Sharma <himanshu.sharma1103@gmail.com>
2020-11-01 17:33:55 +0530

    Fix filter logic for stubs

--- src/components/FilterableStubList/StubList/StubList.js
+++ src/components/FilterableStubList/StubList/StubList.js
@@ -4,18 +4,23 @@ import "./StubList.css";
 const StubList = (props) => {
   const filterText = props.filterText;
   console.log(`filter text is: ${filterText}`);
+  const stubs = [];
+  props.stubs.forEach((stub) => {
+    if (
+      stub.predicates[0].equals.method.includes(filterText) ||
+      stub.predicates[0].equals.path.includes(filterText)
+    ) {
+      stubs.push(
+        <li key={stub.id}>
+          <span>{stub.predicates[0].equals.method}</span>
+          <span>{stub.predicates[0].equals.path}</span>
+        </li>
+      );
+    }
+  });
   return (
     <section className="stub-list">
-      <ul>
-        {props.stubs
-          .filter((stub) => stub.predicates[0].equals.path === filterText)
-          .map((stub) => (
-            <li key={stub.id}>
-              <span>{stub.predicates[0].equals.method}</span>
-              <span>{stub.predicates[0].equals.path}</span>
-            </li>
-          ))}
-      </ul>
+      <ul>{stubs}</ul>
     </section>
   );
 };
